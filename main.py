24926e9868c0dc413f6f59cedb0e4602b0d7e04e
RonaldyK <rmoh0035@student.monash.edu>
2024-04-06 02:55:15 +0800

    Added functionality to show and edit parameters in maintanance mode

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 userChoice = 0
 settingPin = 1234
 ultraSensorCutoff = 20
-
+distanceMeasured = []
 
 def setup():
     global userChoice
@@ -35,7 +35,6 @@ def display_service_menu():
           "3.Maintenance Adjustment Mode\n"
           )
     
-
     while True:
         try:
             global userChoice
@@ -52,34 +51,63 @@ def display_service_menu():
             exit()
 
 def maintenance_adjustment_mode():
+    global ultraSensorCutoff
+    userAuth = 0
     try:
-        print("Entered Maintenance Mode")
-        # Input validation for user pin.
         while True:
-            userPin = int(input("Please enter pin: "))
-            if userPin == settingPin:
-                print("Gained access!")
-                break
-            else:
-                print("Incorrect pin!")
-        # Main logic  for maintenance adjustment mode
-        print("Available parameters"
-              f"Ultrasound Cutoff Distance: {ultraSensorCutoff}")
+            print("Entered Maintenance Mode")
+            # Input validation for user pin.
+            while userAuth == 0:
+                userPin = int(input("Please enter pin: "))
+                if userPin == settingPin:
+                    print("Gained access!\n")
+                    userAuth = 1
+                else:
+                    print("Incorrect pin!\n")
+            # Main logic  for maintenance adjustment mode
+            print("Available parameters\n"
+                 f"1. Ultrasound Cutoff Distance: {ultraSensorCutoff}cm\n"
+                  "0. Exit Maintenance Mode\n")
+            userEdit = int(input("Please select which parameter you want to edit: "))
+            if userEdit == 0:
+                print("Exiting Maintenance Mode...")
+                setup()
+                return
+            if userEdit == 1:
+                print("Editing the ultrasonic sensor cutoff distance...\n "
+                    "Please select an integer from 1 - 20 cm\n"
+                     )
+                while True:
+                    try:
+                        userEditValue = int(input("Editing: "))
+                        if 1 <= userEditValue <= 20:
+                            ultraSensorCutoff = userEditValue
+                            print(f"Ultrasound cutoff distance set to: {ultraSensorCutoff}cm")
+                            break
+                        else:
+                            print("Please enter an integer between 1 and 20.")        
+                    except ValueError:
+                        print("Please enter a valid integer.")    
     except ValueError:
         print("Please enter a valid integer.")
     except KeyboardInterrupt:
         print("\n")
-        display_service_menu()
+        setup()
 
 
 
 def normal_operation_mode():
     try:
+        global distanceMeasured
+        startTime = time.time()
         print("I'm in normal op mode")
-        read_sensor()
+        distanceMeasured.append(read_sensor())
+        print(f"Distance to nearest vehicle: {distanceMeasured}cm")
+        endTime = time.time()
+        print(f"Polling loop time: {startTime - endTime}sec")
     except KeyboardInterrupt:
         print("\n")
-        display_service_menu()
+        setup()
 
 
 
@@ -88,13 +116,12 @@ def data_observation_mode():
         print("I'm in data op mode")
     except KeyboardInterrupt:
         print("\n")
-        display_service_menu()
+        setup()
 
 def read_sensor():
     rawDistances = []
     while True:
         sensorReading = random.randint(1, 30)
-        print(sensorReading)
         if sensorReading <= ultraSensorCutoff:
             rawDistances.append(sensorReading)
         else:
@@ -105,12 +132,12 @@ def read_sensor():
             rawDistances.pop(0)
         print(rawDistances)
         if len(rawDistances) == 4:    
-            averageDistances = sum(rawDistances) / 4
-            print(f"Average ultrasonic sensor reading: {averageDistances}cm")
-            return averageDistances
+            averageDistance = sum(rawDistances) / 4
+            print(f"Average ultrasonic sensor reading: {averageDistance}cm")
+            return averageDistance
         else:
             print("Insufficient data points.")
-        time.sleep(2)
+        time.sleep(0.25)
 
 
 if __name__ == '__main__':
@@ -118,5 +145,3 @@ if __name__ == '__main__':
     loop()
 
 
-def pull_request():
-    pass
